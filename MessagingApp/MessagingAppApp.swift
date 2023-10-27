1fea5501c08e80fa53501a872b7ab4bf4f267c4b
lorenzom <lorenzo.masucci@thetrainline.com>
2023-10-27 14:35:20 +0100

    Initial Commit

---
+++ MessagingApp/MessagingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessagingAppApp.swift
+//  MessagingApp
+//
+//  Created by Lorenzo Masucci on 27/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MessagingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
